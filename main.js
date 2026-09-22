8b9ab755bfdca440b7da44c56c4f6e6a6042d175
Nezox212 <nezox0903@gmail.com>
2026-09-22 12:52:50 +0600

    update project

--- main.js
+++ main.js
@@ -1,88 +1,156 @@
-//1 Создай три числа и найди среди них самое большое
-const firstNumber = +prompt("напишите первое число")
-const secondNumber = +prompt("напишите второе число")
-const thirdNumber = +prompt("напишите третье число")
-if(firstNumber > secondNumber && firstNumber > thirdNumber){ console.log(firstNumber + " больше чем " + secondNumber + " и " + thirdNumber); }
-else if(secondNumber > firstNumber && secondNumber > thirdNumber) {console.log(secondNumber + " больше чем " + firstNumber + " и " + thirdNumber); }
-else if (thirdNumber > firstNumber && thirdNumber > secondNumber ) {console.log(thirdNumber + " больше чем " + firstNumber + " и " + secondNumber); }
-else (console.log("Ваши числа одинаковые " + firstNumber + ", "  + secondNumber + ", " +  thirdNumber));
+// Циклы
+//1 Вывести числа от 1 до 20
+for(let i =1; i <= 20;i++) console.log(i);
+//2 Вывести числа от 20 до 1.
+for(let i =20; i >= 1 ;i--) console.log(i);
+//3 Вывести все чётные числа от 1 до 50.
+for(let i = 1;i <= 50;i++) {
+    if(i % 2 == 0){console.log(i);}
+}
+//4 Посчитать сумму чисел от 1 до 100.
+let i = 1
+let summ = 0
+while(i <= 100){
+    summ += i
+    i++
+}
+console.log(summ)
+// 5 - Вывести квадраты чисел от 1 до 10.
+let i1 = 1
+let square = 1
+
+while(i1 <= 10){
+    square = i1
+    square **= 2
+    console.log("Квадрат " + i1 + " = " + square)
+    i1++
+}
+
+//6 Вывести таблицу умножения для числа 5.
+let i2 = 1
+let five = 5
+while(i2 <= 10){
+    console.log(i2 + " * " + " 5 " + " = " + five * i2)
+    i2++
+}
+
+//7 - Посчитать сумму всех чётных чисел от 1 до 50.
+let i3 = 1
+let summ1 = 0
+
+while(i3 <= 50){
+    if(i3 % 2 === 0){
+        summ1 += i3
+    }
+    i3++
+
+
+}console.log(summ1)
+
+
+//8 - Вывести все числа от 0 до 100 с шагом 5.
+for(let i = 0 ; i <= 100; i += 5) console.log(i)
+
+//9 - Посчитать, сколько чисел от 1 до 100 делятся на 7 и вывести количество
+let count = 0
+for(let i = 1; i <= 100; i++) {
+    if (i % 7 == 0) {
+        console.log(i)
+        count++
+    }
+}console.log("Общее количество цифр делящихся на 7 : " + count)
+
+//Объекты
+//      - Создать объект user с полями name, age, city и вывести их.
+const user = {
+    name: "Антон",
+    age: 23,
+    city: "Москва"
+}
+console.log(user);
+
+//- Изменить значение свойства age.
+
+user.age = 30;
+
+//- Добавить свойство phone.
+
+user.phone = "iphone"
+console.log(user);
+
+//- Удалить свойство city.
+
+delete user.city
+
+//- Проверить наличие свойства email с помощью оператора in.
+console.log("mail" in user)
+
+// - Вывести все ключи объекта через цикл
+// - Вывести все значения объекта через цикл
+
+for (key in user) {
+    console.log(key)
+    console.log(user[key])
+}
+//- Посчитать количество свойств объекта
+console.log(Object.keys(user).length)
 
 
 
-//2 Проверь, находится ли возраст в диапазоне от 18 до 60 лет включительно
-const age = +prompt("Сколько вам лет?")
-if(age >= 18 && age <= 60){console.log(true)}
-else(console.log(false))
+//Массивы
+
+//- Создать массив из 5 чисел и вывести каждый элемент через цикл.
+
+let mass = [1,2,3,5,4,19,-1,-10]
+
+for (let i = 0; i < mass.length; i++) {
+    console.log(mass[i])
+}
+
+//- Найти сумму всех элементов массива.
+let massSumm = 0
+for (let i = 0; i < mass.length; i++) {
+    massSumm += mass[i]
+}console.log("сумма чисел массива = " + massSumm)
+
+//- Найти максимальный элемент массива.
+let max = mass[0];
+
+for (let i = 1; i < mass.length; i++) {
+    if (mass[i] > max) {
+        max = mass[i];
+    }
+}
+console.log( "Наибольшее число в массиве : " +  max)
+
+//- Найти минимальный элемент массива.
+
+let min = mass[0];
+
+for (let i = 1; i < mass.length; i++) {
+    if (mass[i] < min) {
+        min = mass[i];
+    }
+}
+console.log( "Наименьшее число в массиве : " +  min)
+
+//- Посчитать сумму только чётных элементов массива.
+let sumOfEven = 0
+for (let i = 0; i < mass.length; i++) {
+    if(mass[i] % 2 == 0){ sumOfEven += mass[i] }
+
+}
+console.log("Сумма четных чисел массива =  " + sumOfEven)
 
 
+//- Посчитать количество отрицательных чисел в массиве.
+let negNum = 0
+for (let i = 0; i < mass.length; i++) {
+    if (mass[i] < 0 ){ negNum += mass[i] }
+}console.log("Сумма негативных чисел массива = " + negNum)
 
-//3Создай константы username и password, затем попроси у пользователя ввести username и пароль.
-// Eсли пользователь ввёл данные корректно - выведи “Вы успешно вошли в систему”,
-// а если нет то выведи “Некорректный username или пароль”
-const username = "ruslan"
-const password = +"123456"
-
-const name = prompt("Введите ваш username")
-const pass = +prompt("Введите ваш password")
-if (username === name && password === pass) {console.log("Вы успешно вошли")}
-else (console.log("Вход запрешен , неверный логин или пароль"))
-
-
-//4 Создай тест из 5 вопросов и создай 5 констант с правильными ответами. Попроси у пользователя ввести 5 ответов на
-// всё 5 вопросов и в конце выведи количество правильных ответов пользователя
-const firstQuestion = "Лондон"
-const secondQuestion = "Меркурий"
-const thirdQuestion = +"60"
-const fourthQuestion = "AND"
-const fifthQuestion = +"2"
-
-const firstAnswer = prompt("Столица Великобритании");
-const secondAnswer = prompt("Самая близкая планета к Солнцу")
-const thirdAnswer = +prompt("15 * 4 ?")
-const fourthAnswer = prompt("Что означает && в JS ? ")
-const fifthAnswer = +prompt("4 под корнем")
-
-if(firstQuestion == firstAnswer ){console.log("1. " + firstAnswer + " Правильно!")}
-else(console.log( "1. " + firstAnswer + " неправильно): " + "               правильный ответ:" + firstQuestion))
-
-if(secondQuestion == secondAnswer){console.log("2. " + secondAnswer + " Правильно!")}
-else(console.log( "2. " + secondAnswer + " неправильно): " + "               правильный ответ:" + secondQuestion))
-
-if(thirdQuestion == thirdAnswer){console.log("3. " + thirdAnswer + " Правильно!")}
-else(console.log( "3. " + thirdAnswer + " неправильно): " + "               правильный ответ:" + thirdQuestion))
-
-if(fourthQuestion == fourthAnswer){console.log("4. " + fourthAnswer + " Правильно!")}
-else(console.log( "4. " + fourthAnswer + " неправильно): " + "               правильный ответ:" + fourthQuestion))
-
-if(fifthQuestion == fifthAnswer){console.log("5. " + fifthAnswer + " Правильно!")}
-else(console.log( "5. " + fifthAnswer + " неправильно): " + "               правильный ответ:" + fifthQuestion))
-
-
-
-
-
-//5 Если число делится на 2 или на 7, выведи "Подходит" а иначе “Не подходит”
-const twoAndSevenNumber = +prompt("Введите число")
-if (twoAndSevenNumber % 2 === 0 && twoAndSevenNumber % 7 === 0 ) {console.log("Подходит")}
-else(console.log("Не подходит"))
-
-
-//6 Если сумма покупки больше 5000, предоставь скидку 10% и выведи результат, а иначе выведи саму сумму.
-const price = +prompt("Введите сумму покупки")
-if(price >= 5000){console.log("итоговая сумма со скидкой: " + price * 0.9 )}
-else(console.log("итоговая сумма без скидки: " + price))
-
-
-//7 Проверь, является ли символ гласной буквой (a, e, i, o, u). ( true | false )
-const letter = prompt("Введите любую английскую букву");
-if (letter === "a" || letter === "e" || letter === "o"  || letter === "i" || letter === "o") {console.log(true)}
-else console.log(false)
-
-
-
-//8 Попроси у пользователя число и процент, затем вычисли данный процент от данного числа
-const number = +prompt("Введите любое число")
-const percent = prompt("Введите процент для вычисления")
-console.log( percent * 0.01 * number)
-
-
+//- Найти произведение всех элементов массива.
+let multiply = 1
+for (let i = 0; i < mass.length; i++) {
+    multiply *= mass[i]
+}console.log("произведение все чисел массива = " + multiply)
